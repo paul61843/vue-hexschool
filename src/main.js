5291d198032ebe7ae8653e841ef397946242f3e4
helio <paul61843@gmail.com>
2021-03-10 21:19:39 +0800

    feat: 加入 vue-router axios bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import BootstrapVue from 'bootstrap-vue'
+import router from './router'
+
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
